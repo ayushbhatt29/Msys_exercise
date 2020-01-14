3592b86d1a9dfd72c662e68e467e5d7e722044dd
ayushbhatt29 <59819493+ayushbhatt29@users.noreply.github.com>
2020-01-14 17:46:49 +0530

    Add files via upload

---
+++ modules_2.rb
@@ -0,0 +1,23 @@
+module Mymodule
+  def test
+    puts "Hello"
+  end
+  def mymethod
+    puts "My method"
+  end
+end
+
+module Mymodule
+  def newmethod
+    puts "This is new method"
+  end
+end
+
+include Mymodule
+Mymodule.test
+
+module Mymodule
+  remove_method :mymethod
+end
+include Mymodule
+Mymodule.mymethod
