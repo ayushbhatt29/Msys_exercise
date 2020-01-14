769c6ad00e8584ed70b249b9b65b434acdc0f268
ayushbhatt29 <59819493+ayushbhatt29@users.noreply.github.com>
2020-01-14 15:56:33 +0530

    Update exception_handling.rb

--- exception_handling.rb
+++ exception_handling.rb
@@ -1,10 +1,12 @@
 lucky_num=[10,20,30,40]
 begin
-#  num=10/0
-  puts lucky_num["dog"]
+ num=10/0
+#  puts lucky_num["dog"]
 rescue ZeroDivisionError=>e
   puts e
 rescue TypeError
 puts "wrong type"
+rescue 
+  puts "Exception occured"
 
 end
