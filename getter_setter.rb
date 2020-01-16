9e612bf85db41ff5c60e076545d25833b6775be9
ayushbhatt29 <59819493+ayushbhatt29@users.noreply.github.com>
2020-01-16 15:09:31 +0530

    Add files via upload

---
+++ getter_setter.rb
@@ -0,0 +1,18 @@
+class Cswebsite
+#instance variable
+  def initialize(website)
+    @website=website
+  end
+#getter method
+  def website
+    @website
+  end
+#setter method
+  def website=(website)
+    @website=website
+  end
+end
+g=Cswebsite.new "google.com"
+p g.website
+g.website="youtube.com"
+p g.website
